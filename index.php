58f84821a85b6931f004ad33516d86ab080e9dd8
= <=>
2020-04-14 20:16:26 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Dipole | Coming Soon</title>
+        <link rel="stylesheet" href="coming_soon.css" type="text/css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="lib/popups/popups.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link rel="shortcut icon" href="logo.png" />
+    </head>
+
+    <body>
+        <header>
+            <main>
+                <div class="container-fluid">
+                    <div id="main" class="row">
+                        <div class="col-12 text-center">
+                            <div class="col-6 col-lg-1 ml-auto mr-auto mt-5">
+                                <img class="img-fluid" src="logo.png"/>
+                            </div>
+                            <h4 class="text-light mt-4">Something cool is going to go here</h4>
+                            <p class="text-light mt-4">Sadly, it's just not ready to go here yet</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </header>
+    </body>
+</html>
+
+<?php
+    sql_close($conn);
+?>
